025eb0e675b6a1488b91040e9a20eb0950696f33
bulkaaaaaa <106598740+bulkaaaaaa@users.noreply.github.com>
2024-03-25 20:12:34 +0200

    Add files via upload

---
+++ WebApplicationn8/WebApplicationn8/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using WebApplicationn8.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebApplicationn8.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = new List<Product>
+            {
+                new Product {ID = 1, Name = "iPhone 13", Price = 899.00m, CreatedDate = DateTime.Now },
+                new Product {ID = 2, Name = "MacBook Air", Price = 1199.00m, CreatedDate = DateTime.Now.AddMinutes(10) },
+                new Product {ID = 3, Name = "iPad Air", Price = 649.00m, CreatedDate = DateTime.Now.AddMinutes(20) },
+                new Product {ID = 4, Name = "Apple Watch Series 7", Price = 459.00m, CreatedDate = DateTime.Now.AddMinutes(30) },
+                new Product {ID = 5, Name = "Amazon Kindle Paperwhite", Price = 129.99m, CreatedDate = DateTime.Now.AddMinutes(40) },
+                new Product {ID = 6, Name = "PlayStation 5", Price = 599.00m, CreatedDate = DateTime.Now.AddMinutes(50) },
+                new Product {ID = 7, Name = "Fitbit Charge 5", Price = 179.95m, CreatedDate = DateTime.Now.AddHours(1) }
+
+            };
+
+            return View(products);
+        }
+    }
+}
---
+++ WebApplicationn8/WebApplicationn8/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebApplicationn8.Models
+{
+    public class Product
+    {
+        public int ID { get; set; } = 0;
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; } = decimal.Zero;
+        public DateTime CreatedDate { get; set; } = DateTime.MinValue;
+    }
+}
